57570840ee0eb0a5c05657410ba2a9a8492d9c12
urdhanaka <urdhanaka.aptanagi@gmail.com>
2025-06-19 04:58:03 +0700

    feat: updates

---
+++ program/xml-cloud-init-iso.go
@@ -0,0 +1,30 @@
+func createBase(
+    instanceName string,
+    instanceConfig *virtualization_model.CreateInstanceRequest,
+) (string, error) {
+    ...
+    seedFile := POOL_DIR + "/" + instanceName + ".iso"
+    ...
+
+    domConfig := &libvirtxml.Domain{
+        ...
+        Devices: &libvirtxml.DomainDeviceList{
+            Emulator: "/usr/bin/qemu-system-x86_64",
+            Disks: []libvirtxml.DomainDisk{
+                ...
+                {
+                    ...
+                    Source: &libvirtxml.DomainDiskSource{
+                        File: &libvirtxml.DomainDiskSourceFile{
+                            File: seedFile,
+                        },
+                    },
+                    ...
+                },
+            },
+            ...
+        },
+        ...
+    }
+    ...
+}
